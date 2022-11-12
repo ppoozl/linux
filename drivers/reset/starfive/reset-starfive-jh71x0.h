84e0f575cef119025d636e858098b0c9cf062d7e
Hal Feng <hal.feng@starfivetech.com>
2022-11-12 23:39:59 +0800

    reset: starfive: Add StarFive JH7110 reset driver
    Add auxiliary driver to support StarFive JH7110 system
    and always-on resets.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Hal Feng <hal.feng@starfivetech.com>

--- drivers/reset/starfive/reset-starfive-jh71x0.h
+++ drivers/reset/starfive/reset-starfive-jh71x0.h
@@ -6,6 +6,12 @@
 #ifndef __RESET_STARFIVE_JH71X0_H
 #define __RESET_STARFIVE_JH71X0_H
 
+struct reset_info {
+	unsigned int nr_resets;
+	unsigned int assert_offset;
+	unsigned int status_offset;
+};
+
 int reset_starfive_jh71x0_register(struct device *dev, struct device_node *of_node,
 				   void __iomem *assert, void __iomem *status,
 				   const u32 *asserted, unsigned int nr_resets,

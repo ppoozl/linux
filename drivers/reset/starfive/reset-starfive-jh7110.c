6be652d7527728240f9a56ce18c30e286ffc8a69
Xingyu Wu <xingyu.wu@starfivetech.com>
2023-01-20 10:44:42 +0800

    reset: starfive: jh7110: Add StarFive Video-Output reset support
    Add auxiliary_device_id to support StarFive JH7110 Video-Output resets
    of which the auxiliary device name is "clk_starfive_jh71x0.reset-vout".
    
    Signed-off-by: Xingyu Wu <xingyu.wu@starfivetech.com>

--- drivers/reset/starfive/reset-starfive-jh7110.c
+++ drivers/reset/starfive/reset-starfive-jh7110.c
@@ -52,6 +52,12 @@ static const struct reset_info jh7110_isp_info = {
 	.status_offset = 0x3C,
 };
 
+static const struct reset_info jh7110_vout_info = {
+	.nr_resets = JH7110_VOUTRST_END,
+	.assert_offset = 0x48,
+	.status_offset = 0x4C,
+};
+
 static const struct auxiliary_device_id jh7110_reset_ids[] = {
 	{
 		.name = "clk_starfive_jh71x0.reset-sys",
@@ -68,6 +74,10 @@ static const struct auxiliary_device_id jh7110_reset_ids[] = {
 	{
 		.name = "clk_starfive_jh71x0.reset-isp",
 		.driver_data = (kernel_ulong_t)&jh7110_isp_info,
+	},
+	{
+		.name = "clk_starfive_jh71x0.reset-vout",
+		.driver_data = (kernel_ulong_t)&jh7110_vout_info,
 	},
 	{ /* sentinel */ }
 };
